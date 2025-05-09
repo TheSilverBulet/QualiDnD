822e2a429f5ebba316f2b8b3d87347cca5233d57
TheSilverBulet <bcebulski96@gmail.com>
2025-05-09 19:50:17 -0400

    entryComponents are apparently no longer required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,16 +88,16 @@ import { NotificationComponent } from './notification/notification.component';
   bootstrap: [AppComponent],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
-  ],
-  entryComponents: [
-    DashboardCharacterExpandedDialogComponent,
-    CharacterCreationDialogComponent,
-    ResetPasswordDialogComponent,
-    UpdateAttributeDialogComponent,
-    DiceRollerDialogComponent,
-    ReferenceDialogComponent,
-    EditCharacterDialogComponent,
-    InitiativeDetailsDialogComponent
-  ]
+  ]//,
+  // entryComponents: [
+  //   DashboardCharacterExpandedDialogComponent,
+  //   CharacterCreationDialogComponent,
+  //   ResetPasswordDialogComponent,
+  //   UpdateAttributeDialogComponent,
+  //   DiceRollerDialogComponent,
+  //   ReferenceDialogComponent,
+  //   EditCharacterDialogComponent,
+  //   InitiativeDetailsDialogComponent
+  // ]
 })
 export class AppModule { }
