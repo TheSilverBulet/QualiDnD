d968531189416b50c130be63d1f963bfc0a51074
Ben <bcebulski96@gmail.com>
2025-05-22 12:58:33 -0400

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    // Add other client-side providers (e.g., HttpClientModule, animations)
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
